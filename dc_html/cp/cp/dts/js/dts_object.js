de27d77cb65ab59c6d567aefe6b60d2853ac4112
fankunet-arch <fankunet@gmail.com>
2025-11-20 00:00:45 +0100

    Delete dc_html/cp/cp directory

--- dc_html/cp/cp/dts/js/dts_object.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Toast 提示
-    if ($('#feedback-bar').length) {
-        setTimeout(function() {
-            $('#feedback-bar').fadeOut();
-        }, 3000);
-    }
-});
