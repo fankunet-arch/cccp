c5e6a905b34c0b6f74c9af5725d57a1e3282636e
fankunet-arch <fankunet@gmail.com>
2025-11-20 00:03:11 +0100

    Delete app/cp/cp directory

--- app/cp/cp/dts/actions/dts_event_save.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Legacy wrapper for older forms posting to action=dts_event_save.
- * Delegates to the consolidated dts_ev_save handler.
- */
-require_once __DIR__ . '/dts_ev_save.php';
-
